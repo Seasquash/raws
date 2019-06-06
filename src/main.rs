90d86fc184dc5e04726f0ce6420bbc37c7d88390
Justin Lam <4069123+ninth-dev@users.noreply.github.com>
2019-06-06 18:31:28 +1000

    Add in sqs and list-queues command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+  let authors : &str = &vec!("Justin Lam", "Paolo Napolitano").join(", ");
+  let matches =
+    App::new("raws")
+      .version("0.1")
+      .author(authors)
+      .about("AWS SQS stuff")
+      .subcommand(SubCommand::with_name("sqs")
+        .subcommand(SubCommand::with_name("list-queues"))
+      )
+      .get_matches();
+
+   if let Some(matches) = matches.subcommand_matches("sqs") {
+        println!("sqs");
+        if let Some(_) = matches.subcommand_matches("list-queues") {
+          println!("list queues");
+        } else {
+          println!("...");
+        }
+    }
 }
