408395736043b720429b9d6063d6bb8283f6e92a
ninth-dev <4069123+ninth-dev@users.noreply.github.com>
2019-09-11 17:15:32 +1000

    --wip-- [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
-use clap::{App, SubCommand, ArgMatches};
+use clap::{App, ArgMatches, SubCommand};
 use rusoto_core::Region;
 use rusoto_core::RusotoError;
 use rusoto_sqs::*;
-
+// use std::error::Error;
+use failure::Error;
 
 // subcommand_handler.... for sqs only
-fn sqs_subcommand_handler<T>(arg_matches : &ArgMatches<'_>) -> Result<(), RusotoError<T>> {
+fn sqs_subcommand_handler(sqs: SqsClient, arg_matches: &ArgMatches<'_>) -> Result<(), Error> {
     match arg_matches.subcommand_name() {
-        Some("list-queues") => list_queue_handler<T>(),
-        Some("list-messages") => list_message_handler(),
-        _ => unimplemented!()
+        Some("list-queues") => Ok(list_queue_handler(sqs)?),
+        Some("list-messages") => Ok(list_message_handler(sqs)?),
+        _ => unimplemented!(),
     }
 }
-fn list_queue_handler<T : ListQueuesError>() -> Result<(), RusotoError<T>> {
-    unimplemented!()
+
+fn list_queue_handler(sqs: SqsClient) -> Result<(), RusotoError<ListQueuesError>> {
+    let request = ListQueuesRequest::default();
+    Ok(sqs
+        .list_queues(request)
+        .sync()?
+        .queue_urls
+        .unwrap_or_default()
+        .iter()
+        .map(|url| url.split("/").last().unwrap_or_default())
+        .map(|name| println!("{}", name))
+        .collect())
 }
 
-fn list_message_handler() -> Result<(), RusotoError<ReceiveMessageError>> {
+fn list_message_handler(sqs: SqsClient) -> Result<(), RusotoError<ReceiveMessageError>> {
     unimplemented!()
 }
 
 // list-queues handler
 // list-message handler
 // download-message handler
-
 
 fn main() {
     let authors: &str = &vec!["Justin Lam", "Paolo Napolitano"].join(", ");
@@ -39,61 +49,62 @@ fn main() {
         )
         .get_matches();
 
-
     // list-queues
     // list-messages <queue-url>
     // download-messages <queue-url>
+    let sqs = SqsClient::new(Region::ApSoutheast2);
     if let Some(sqs_matches) = matches.subcommand_matches("sqs") {
-        let sqs = SqsClient::new(Region::ApSoutheast2);
-
-        match sqs_matches.subcommand_name() {
-            Some("list-queues") => {
-                let request = ListQueuesRequest::default();
-                let result = sqs.list_queues(request).sync();
-                match result {
-                    Ok(list_queues_results) => list_queues_results
-                        .queue_urls
-                        .unwrap_or_default()
-                        .iter()
-                        .map(|url| url.split("/").last().unwrap_or_default())
-                        .map(|name| println!("{}", name))
-                        .collect(),
-                    // TODO extract the Message from the error
-                    Err(rusoto_error) => {
-                        dbg!(rusoto_error);
-                    }
-                }
+        match sqs_subcommand_handler(sqs, sqs_matches) {
+            Err(e) => {
+                dbg!(e);
+                ()
             }
-            Some("list-messages") => {
-                let request = ReceiveMessageRequest{
-                    queue_url: String::from("https://sqs.ap-southeast-2.amazonaws.com/954088256298/rust-aws-integration"),
-                    max_number_of_messages: Some(10),
-                    ..Default::default()
-                };
-                let result = sqs.receive_message(request).sync();
-                match result {
-                    Ok(message_results) => message_results
-                        .messages
-                        .unwrap_or_default()
-                        .iter()
-                        .map(|message| {
-                            if let Some(x) = &message.body {
-                                println!("{}", x);
-                            }
-                        })
-                        .collect(),
-                    Err(rusoto_error) => {
-                        dbg!(rusoto_error);
-                    }
-                }
-            },
-            Some("download-messages") => println!("download-messages"),
-            _ => println!("NOT ALLOWED"),
+            _ => (),
         }
-        // if let Some(_) = sqs_matches.subcommand_matches("list-queues") {
-        //   println!("list queues");
-        // } else {
-        //   println!("...");
-        // }
     }
+    // match sqs_matches.subcommand_name() {
+    //     Some("list-queues") => {
+    //         let request = ListQueuesRequest::default();
+    //         let result = sqs.list_queues(request).sync();
+    //         match result {
+    //             Ok(list_queues_results) => list_queues_results
+    //                 .queue_urls
+    //                 .unwrap_or_default()
+    //                 .iter()
+    //                 .map(|url| url.split("/").last().unwrap_or_default())
+    //                 .map(|name| println!("{}", name))
+    //                 .collect(),
+    //             // TODO extract the Message from the error
+    //             Err(rusoto_error) => {
+    //                 dbg!(rusoto_error);
+    //             }
+    //         }
+    //     }
+    //     Some("list-messages") => {
+    //         let request = ReceiveMessageRequest{
+    //             queue_url: String::from("https://sqs.ap-southeast-2.amazonaws.com/954088256298/rust-aws-integration"),
+    //             max_number_of_messages: Some(10),
+    //             ..Default::default()
+    //         };
+    //         let result = sqs.receive_message(request).sync();
+    //         match result {
+    //             Ok(message_results) => message_results
+    //                 .messages
+    //                 .unwrap_or_default()
+    //                 .iter()
+    //                 .map(|message| {
+    //                     if let Some(x) = &message.body {
+    //                         println!("{}", x);
+    //                     }
+    //                 })
+    //                 .collect(),
+    //             Err(rusoto_error) => {
+    //                 dbg!(rusoto_error);
+    //             }
+    //         }
+    //     },
+    //     Some("download-messages") => println!("download-messages"),
+    //     _ => println!("NOT ALLOWED"),
+    // }
+    // }
 }
