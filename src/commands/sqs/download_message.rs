0ad67d85307837f3d1957132e9c3224e9b7dc142
Paolo Napolitano <>
2020-02-16 22:10:53 +1100

    Storing messages to a file

--- src/commands/sqs/download_message.rs
+++ src/commands/sqs/download_message.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 use rusoto_sqs::*;
 use super::common::construct_queue_url;
 use super::models::Message::RawsMessage;
@@ -11,8 +13,11 @@ pub fn handler(sqs: SqsClient, queue_name: &str) -> Result<Vec<String>, Box<dyn 
     ..Default::default()
   };
   let messages = retrieve_all_messages(&sqs, &request, vec!());
-  // For each message, get the receipt handle, call the delete message
-  // and print the message body
+  // For each message, get the receipt handle, call the delete message,
+  // print the message body and save it to a file
+  let path = "sqs_messages.txt";
+  let mut output = File::create(path)?;
+
   messages
     .iter()
     .map(|m: &RawsMessage| {
@@ -22,7 +27,9 @@ pub fn handler(sqs: SqsClient, queue_name: &str) -> Result<Vec<String>, Box<dyn 
           let deleted = delete_message(&sqs, queue_name, receipt.into());
           match deleted {
             Ok(()) => {
-              println!("Deleted message: {}", msg.body.unwrap_or("NO MESSAGE FOUND".into()));
+              let text_to_write = msg.body.unwrap_or("NO MESSAGE FOUND".into());
+              write!(output, "{}\n\n", text_to_write);
+              println!("Deleted message: {}", text_to_write);
             },
             _ => {
               println!("Failed to delete message: {}", msg.body.unwrap_or("NO MESSAGE FOUND".into()));
