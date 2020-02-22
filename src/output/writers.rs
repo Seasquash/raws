b072ae6033094f428c83c0f70a4a63b476958357
Paolo Napolitano <>
2020-02-22 15:07:18 +1100

    Fixing output formatter and adding tests

--- src/output/writers.rs
+++ src/output/writers.rs
@@ -4,6 +4,6 @@ use std::fs::File;
 pub fn write_to_file(mut output: &File, text_to_write: &str) {
   match write!(output, "{}\n\n", text_to_write) {
     Ok(_) => (),
-    Err(e) => println!("Failed to write tio file, with error: {}", e)
+    Err(e) => println!("Failed to write to file, with error: {}", e)
   }
 }
